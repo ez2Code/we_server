2d7fa81fd4cf1e732cc19cc901749d42f4a3ff7c
Levy <levy.cn@outlook.com>
2017-02-11 15:15:55 +0800

    joke访问顺序由随即改为顺序

--- handler/jokeHandler.js
+++ handler/jokeHandler.js
@@ -4,6 +4,7 @@
 
 var fs = require('fs');
 var config = require('../config/config');
+var index=0;
 
 var getJoke = function (callback) {
     fs.readdir(config.jokeDir, function (err, files) {
@@ -12,7 +13,11 @@ var getJoke = function (callback) {
             return;
         }
         var count = files.length;
-        var index = new Date().getTime()%count;
+        if(index<count-1){
+            index+=1;
+        }else {
+            index=0;
+        }
         fs.readFile(config.jokeDir+files[index],'UTF-8' ,function (err, data) {
             if (err) throw err;
             callback(data);
